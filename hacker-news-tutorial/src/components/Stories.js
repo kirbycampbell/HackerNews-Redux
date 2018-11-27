42355cb42a4825b2fb06ed85785fad116fa90c40
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-27 08:44:58 -0800

    Styling Added

--- hacker-news-tutorial/src/components/Stories.js
+++ hacker-news-tutorial/src/components/Stories.js
@@ -2,10 +2,44 @@ import React from "react";
 import "./Stories.css";
 import Story from "./Story.js";
 
+const COLUMNS = {
+  title: {
+    label: "Title",
+    width: "40%"
+  },
+  author: {
+    label: "Author",
+    width: "30%"
+  },
+  comments: {
+    label: "Comments",
+    width: "10%"
+  },
+  points: {
+    label: "Points",
+    width: "10%"
+  },
+  archive: {
+    width: ""
+  }
+};
+
 const Stories = ({ stories }) => (
   <div className="stories">
+    <StoriesHeader columns={COLUMNS} />
+
     {(stories || []).map(story => (
-      <Story key={story.objectId} story={story} />
+      <Story key={story.objectId} story={story} columns={COLUMNS} />
+    ))}
+  </div>
+);
+
+const StoriesHeader = ({ columns }) => (
+  <div className="stories-header">
+    {Object.keys(COLUMNS).map(key => (
+      <span key={key} style={{ width: COLUMNS[key].width }}>
+        {COLUMNS[key].label}
+      </span>
     ))}
   </div>
 );
