f56337c5f7b499df034b5c6b25b63e6f37ffe3a9
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-27 09:47:57 -0800

    Redux Functionality Working

--- hacker-news-tutorial/src/index.js
+++ hacker-news-tutorial/src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import store from "./store";
+import { getReadableStories } from "./selectors/story";
 import { STORY_ARCHIVE } from "./constants/actionTypes";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <App
-    stories={store.getState().storyState}
-    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
-  />,
-  document.getElementById("root")
-);
+function render() {
+  ReactDOM.render(
+    <App
+      stories={getReadableStories(store.getState())}
+      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />,
+    document.getElementById("root")
+  );
+}
+
+store.subscribe(render);
+render();
 
 serviceWorker.unregister();
